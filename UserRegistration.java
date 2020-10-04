93a8ff08801df4d31bf1496a130ada68edce420b
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-10-04 22:08:39 +0530

    [Jeet Kumar Yadav] Last Name validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,12 +4,22 @@ import java.util.regex.*;
 public class UserRegistration{
 
     public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter  First Name: ");
+
+        System.out.println("Enter First Name: ");
         String firstName = sc.next();
         while(!Pattern.matches("[A-Z]{1}[a-z]{2,}",firstName)){
-            System.out.println("Ivalid input. Enter the First Name: ");
+            System.out.println("Ivalid input. Enter again: ");
             firstName = sc.next();
         }
+
+        System.out.println("Enter Last Name: ");
+        String lastName = sc.next();
+        while(!Pattern.matches("[A-Z]{1}[a-z]{2,}",lastName)){
+            System.out.println("Ivalid input. Enter again: ");
+            lastName = sc.next();
+        }
+
     }
 }
